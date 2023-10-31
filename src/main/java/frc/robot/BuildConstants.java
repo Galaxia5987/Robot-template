e99321020408bc2375d91d21f3f8899616ed8204
eitan <eitan.vichik@gmail.com>
2023-10-31 12:55:55 +0200

    Update gitignore

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot-template";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 12;
-  public static final String GIT_SHA = "295ee5c030b3b9279746bd2966c1ffa5058556fc";
-  public static final String GIT_DATE = "2023-10-31 05:47:15 EDT";
+  public static final int GIT_REVISION = 13;
+  public static final String GIT_SHA = "4c03dcc3403190e740387f7db23aa3832acbafb9";
+  public static final String GIT_DATE = "2023-10-31 06:54:01 EDT";
   public static final String GIT_BRANCH = "feature/2024-template";
-  public static final String BUILD_DATE = "2023-10-31 06:52:46 EDT";
-  public static final long BUILD_UNIX_TIME = 1698749566342L;
+  public static final String BUILD_DATE = "2023-10-31 06:55:27 EDT";
+  public static final long BUILD_UNIX_TIME = 1698749727829L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
