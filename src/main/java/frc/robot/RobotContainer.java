544c2726e76e2d9ba0f1b1f2f0443c86173ebc28
eitan <eitan.vichik@gmail.com>
2023-12-17 19:25:42 +0200

    Revert usage of swerve

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,7 +1,5 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.simulation.XboxControllerSim;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.swerve.SwerveDrive;
 import frc.robot.swerve.commands.KeyboardDriveSim;
@@ -9,14 +7,6 @@ import frc.robot.swerve.commands.KeyboardDriveSim;
 public class RobotContainer {
 
     private static RobotContainer INSTANCE = null;
-
-    static {
-        SwerveDrive.setInstance(Robot.isReal(),
-                Ports.SwerveDrive.DRIVE_IDS,
-                Ports.SwerveDrive.ANGLE_IDS,
-                Ports.SwerveDrive.ENCODER_IDS
-        );
-    }
 
     /**
      * The container for the robot.  Contains subsystems, OI devices, and commands.
@@ -35,7 +25,6 @@ public class RobotContainer {
     }
 
     private void configureDefaultCommands() {
-        SwerveDrive.getInstance().setDefaultCommand(new KeyboardDriveSim());
     }
 
     private void configureButtonBindings() {
