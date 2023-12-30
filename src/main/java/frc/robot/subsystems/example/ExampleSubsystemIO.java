ab0fb5563a91daa88e1f762b95c1341f38f622f7
eitan <eitan.vichik@gmail.com>
2023-12-30 14:32:58 +0200

    Change to autolog

--- src/main/java/frc/robot/subsystems/example/ExampleSubsystemIO.java
+++ src/main/java/frc/robot/subsystems/example/ExampleSubsystemIO.java
@@ -2,13 +2,12 @@ package frc.robot.subsystems.example;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import org.littletonrobotics.junction.LogTable;
-import org.littletonrobotics.junction.inputs.LoggableInputs;
+import org.littletonrobotics.junction.AutoLog;
 
 public interface ExampleSubsystemIO {
 
     // The inputs of the subsystem. This is to be used only in the subsystem classes.
-    ExampleSubsystemInputs inputs = new ExampleSubsystemInputs();
+    ExampleSubsystemInputsAutoLogged inputs = new ExampleSubsystemInputsAutoLogged();
 
     /**
      * Set the position of the subsystem
@@ -29,30 +28,13 @@ public interface ExampleSubsystemIO {
      */
     void updateInputs();
 
-    class ExampleSubsystemInputs implements LoggableInputs {
+    @AutoLog
+    class ExampleSubsystemInputs {
         public Rotation2d angle = new Rotation2d();
         public Translation2d tipPosition = new Translation2d();
         public Rotation2d velocity = new Rotation2d();
 
         public Rotation2d setpointAngle = new Rotation2d();
         public double setpointPower = 0;
-
-        @Override
-        public void toLog(LogTable table) {
-            table.put("Angle", angle.getDegrees());
-            table.put("TipPosition", Translation2d.struct, tipPosition);
-            table.put("Velocity", velocity.getDegrees());
-            table.put("SetpointAngle", setpointAngle.getDegrees());
-            table.put("SetpointPower", setpointPower);
-        }
-
-        @Override
-        public void fromLog(LogTable table) {
-            angle = Rotation2d.fromDegrees(table.get("Angle", angle.getDegrees()));
-            tipPosition = table.get("TipPosition", Translation2d.struct, tipPosition);
-            velocity = Rotation2d.fromDegrees(table.get("Velocity", velocity.getDegrees()));
-            setpointAngle = Rotation2d.fromDegrees(table.get("SetpointAngle", setpointAngle.getDegrees()));
-            setpointPower = table.get("SetpointPower", setpointPower);
-        }
     }
 }
